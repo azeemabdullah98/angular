9621bbdd4f081819c13498c7d264e61dd19b375d
Azeem Abdullah K <azeemquest98@gmail.com>
2025-05-20 08:50:00 +0530

    migration from standalone to module based - mix of standalone and module based- working

--- first-angular-app-2/src/main.ts
+++ first-angular-app-2/src/main.ts
@@ -1,9 +1,15 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import { HeaderComponent } from './app/header/header.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
+
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { AppComponent } from './app/app.component';
+// import { HeaderComponent } from './app/header/header.component';
+
+// bootstrapApplication(AppComponent, appConfig).catch((err) =>
+//   console.error(err)
+// );
 // bootstrapApplication(HeaderComponent);
